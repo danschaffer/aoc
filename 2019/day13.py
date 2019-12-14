8dafa24ba67c7c27d787ac64cbbe59ceafbd55b3
Dan Schaffer <dschaffer@circle.com>
2019-12-14 15:49:25 -0500

    add day13 part 2

--- 2019/day13.py
+++ 2019/day13.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
+import sys
 from intcode import Intcode
 
 class Game:
     def __init__(self, data, input):
         self.intcode = Intcode(data, input)
         self.screen = {}
+        self.rounds = 0
+
+    def find_obj(self, obj):
+        for coord in self.screen.keys():
+            if self.screen[coord] == obj:
+                return coord
 
     def get_blocks(self):
         pieces = []
@@ -44,25 +51,65 @@ class Game:
             screen += '\n'
         print(f"\n{screen}")
 
-    def run_game(self):
+    def run_game(self, manual=False):
         game.intcode.data[0] = 2
+        last_move = ' '
+        score = 0
         while self.intcode.is_running():
+            self.rounds += 1
             self.intcode.run()
             self.draw_screen()
+            ball = self.find_obj(4)
+            paddle = self.find_obj(3)
+            score = self.screen[(-1,0)]
             if (-1,0) in self.screen:
-                print(f"score={self.screen[(-1,0)]}")
-            move_s = input("enter move[left=-1,neutral=0,right=1]: ")
-            if len(move_s) == 0:
-                move = 0
+                print(f"score={score} rounds={self.rounds} ball={ball} paddle={paddle}")
+
+            if manual:
+                move_s = input("enter move[left=-1,,neutral=0,right=1.]: ")
+                if len(move_s) == 0:
+                    move_s = last_move
+                if move_s == ',':
+                    move = -1
+                elif move_s == '.':
+                    move = 1
+                elif move_s == ' ':
+                    move = 0
+                elif move_s == 's':
+                    name = f"day13-{self.rounds}"
+                    data = [self.intcode.data[n] for n in self.intcode.data]
+                    open(name, 'w').write(str(data))
+                    print(f"wrote file {name}")
+                    continue
+                else:
+                    continue
+                last_move = move_s
             else:
-                move = int(move_s)
-            self.intcode.inputs = [move]
+                if ball[0] < paddle[0]:
+                    move = -1
+                elif ball[0] > paddle[0]:
+                    move = 1
+                else:
+                    move = 0
+                self.intcode.inputs = [move]
+        return score
+
 
 if __name__ == '__main__':
-    data = [int(item) for item in open('day13.input').read().strip().split(',')]
+    name = 'day13.input'
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+    contents = open(name).read().strip()
+    if contents.startswith("["):
+        contents = contents[1:]
+    if contents.endswith("]"):
+        contents = contents[:-1]
+    data = [int(item.strip()) for item in contents.split(',')]
     game = Game(data[:], [])
     game.intcode.run()
     print(game.intcode.outputs)
-    print(f"part 1: {game.get_blocks()}")
+    part1 = game.get_blocks()
     game = Game(data[:], [])
-    game.run_game()
+    part2 = game.run_game()
+    print(f"part 1: {part1}")
+    print(f"part 2: {part2}")
