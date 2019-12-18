f18f95c7b556da2de727dcf3b381303b6b441e47
Dan Schaffer <dschaffer@circle.com>
2019-12-18 06:25:16 -0500

    updates with 28 points

--- 2019/day10.py
+++ 2019/day10.py
@@ -2,7 +2,8 @@
 import math
 class Asteroids:
     def __init__(self, input):
-        self.data = {}
+        self.points = []
+        self.results = {}
         self.load(input)
 
     def load(self, input):
@@ -10,44 +11,59 @@ class Asteroids:
         for line in input.strip().split('\n'):
             for x in range(len(line)):
                 if line[x] == '#':
-                    self.data[(x, y)] = 0
+                    self.points += [(x, y)]
             y += 1
 
-    def calculate_angle(self, origin, point1):
-        angle = math.atan2(point1[1] - origin[1], point1[0] - origin[0]) * 180 / math.pi
-        return int(angle)
+    def get_angle(self, a, b):
+        x0, y0 = a
+        x1, y1 = b
+        angle = ((math.atan2(x1 - x0, y1 - y0) * 180 / math.pi) + 360 + 180) % 360
+        return angle
 
-    def distance(self, a,b):
-        return round(math.sqrt((a[0]-b[0])**2 + (a[1] - b[1])**2), 1)
+    def find_angle_results(self):
+        results = {}
+        for p1 in self.points:
+            angles = {}
+            for p2 in self.points:
+                if p1 == p2:
+                    continue
+                angle = self.get_angle(p1, p2)
+                if angle not in angles:
+                    angles[angle] = []
+                angles[angle] += [p2]
+            results[p1] = angles
+        self.results = results
 
-    def is_between(self, a,c,b):
-        return self.distance(a,c) + self.distance(c,b) == self.distance(a,b)
+    def find_best_all(self):
+        self.find_angle_results()
+        results = {}
+        for p in self.results.keys():
+            results[p] = len(self.results[p])
+        sorted_results = sorted(results.items(), key=lambda kv: kv[1], reverse=True)
+        return sorted_results[0]
 
     def find_best(self):
-        matches = {}
-        for point in self.data.keys():
-            num = self.find_match(point)
-            if num not in matches:
-                matches[num] = []
-            matches[num] += [point]
-        maxn = max(matches.keys())
-#        assert(len(matches[maxn]) == 1)
-        return maxn, matches[maxn][0]
+        return self.find_best_all()[1]
 
-    def find_match(self, origin):
-        matches = []
-        for point in self.data.keys():
-            if origin == point:
-                continue
-            found = False
-            for match in matches:
-                if self.is_between(origin, match, point) or self.is_between(origin, point, match):
-                    found = True
-                    continue
-            if not found:
-                matches += [point]
-        return len(matches)
-
+    def vaporize(self, nth):
+        best = self.find_best_all()[0]
+        angles = self.results[best]
+        sorted_angles = sorted(angles.items(), key=lambda kv: kv[0])
+        for angle in sorted_angles:
+            angle[1].sort(key=lambda kv: math.sqrt((best[0] - kv[0])**2 + (best[1] - kv[1])**2))
+        count = 0
+        index = 0
+        # bug angles are backwards
+        match = sorted_angles[len(sorted_angles) - nth + 1][1][0]
+        return 100 * match[0] + match[1]
+        while True:
+            angle0 = sorted_angles[index][1]
+            point0 = angle0[0]
+            if count == nth - 1:
+                return 100 * next[0] + next[1]
+            sorted_angles[index][1].remove(next)
+            index = (index - 1) % len(sorted_angles)
+            count += 1
 
 def test_1():
     input1 = """
@@ -58,10 +74,9 @@ def test_1():
 ...##
 """
     asteroids = Asteroids(input1)
-    assert len(asteroids.data.keys()) == 10
-    maxn, best = asteroids.find_best()
-    assert maxn == 8
-    assert best == (3,4)
+    assert len(asteroids.points) == 10
+    asteroids = Asteroids(input1)
+    assert asteroids.find_best() == 8
 
 
 def test_2():
@@ -78,9 +93,7 @@ def test_2():
 .#....####
 """
     asteroids = Asteroids(input2)
-    maxn, best = asteroids.find_best()
-    assert maxn == 33
-    assert best == (5, 8)
+    assert asteroids.find_best() == 33
 
 def test_3():
     input3 = """
@@ -96,9 +109,7 @@ def test_3():
 .####.###.
 """
     asteroids = Asteroids(input3)
-    maxn, best = asteroids.find_best()
-    assert maxn == 35
-    assert best == (1, 2)
+    assert asteroids.find_best() == 35
 
 def test_4():
     input4 = """
@@ -114,37 +125,67 @@ def test_4():
 .....#.#..
 """
     asteroids = Asteroids(input4)
-    maxn, best = asteroids.find_best()
-    assert maxn == 41
-    assert best == (6, 3)
+    assert asteroids.find_best() == 41
 
-#def test_5():
-#     input5 = """
-# .#..##.###...#######
-# ##.############..##.
-# .#.######.########.#
-# .###.#######.####.#.
-# #####.##.#.##.###.##
-# ..#####..#.#########
-# ####################
-# #.####....###.#.#.##
-# ##.#################
-# #####.##.###..####..
-# ..######..##.#######
-# ####.##.####...##..#
-# .#####..#.######.###
-# ##...#.##########...
-# #.##########.#######
-# .####.#.###.###.#.##
-# ....##.##.###..#####
-# .#.#.###########.###
-# #.#.#.#####.####.###
-# ###.##.####.##.#..##
-# """
-#     asteroids = Asteroids(input5)
-#     maxn, best = asteroids.find_best()
-#     assert maxn == 210
-#     assert best == (11, 13)
+def test_5():
+    input5 = """
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##
+"""
+    asteroids = Asteroids(input5)
+    assert asteroids.find_best() == 210
+
+def test_part2():
+    data = """
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##
+    """
+    asteroids = Asteroids(data)
+    assert asteroids.vaporize(2) == 1201
+    assert asteroids.vaporize(3) == 1202
+    assert asteroids.vaporize(10) == 1208
+    assert asteroids.vaporize(20) == 1600
+    assert asteroids.vaporize(50) == 1609
+    assert asteroids.vaporize(100) == 1016
+    assert asteroids.vaporize(199) == 906
+    assert asteroids.vaporize(200) == 802
+    assert asteroids.vaporize(201) == 1009
 
 if __name__ == '__main__':
     data = """
@@ -174,5 +215,5 @@ if __name__ == '__main__':
 ..###.########.#.###..#.
 """
     asteroids = Asteroids(data)
-    maxn, best = asteroids.find_best()
-    print(f"part 1: {maxn}")
+    print(f"part 1: {asteroids.find_best()}")
+    print(f"part 2: {asteroids.vaporize(200)}")
