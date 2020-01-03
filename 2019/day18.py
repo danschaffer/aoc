2e2a7e3ed55a02f07279dfcae46d7f4064d97728
Dan Schaffer <dschaffer@circle.com>
2020-01-03 09:03:05 -0500

    day18 p1

--- 2019/day18.py
+++ 2019/day18.py
@@ -6,55 +6,29 @@ class Vault:
         self.keys = []
         self.key_locations = {}
         self.map = {}
-        self.doors = {}
         self.paths = {}
         self.cache = []
         self.parse_map(text.strip())
+        self.longest = 0
         self.start = time.time()
 
-    def collect_keys(self, moves):
-        keys = []
-        for move in moves:
-            key = self.map[move]
-            if self.is_key(key) and key not in keys:
-                keys += [key]
-        return keys
+    def parse_map(self, text):
+        y = 0
+        for line in text.split('\n'):
+            x = 0
+            for item in list(line):
+                if re.search('[A-Za-z.@]', item):
+                    self.map[(x,y)] = item
+                x += 1
+            y += 1
 
-    def keys_left(self, keys):
-        keys_left = self.keys[:]
-        for key in keys:
-            keys_left.remove(key)
-        return keys_left
-
-    def valid_path(self, moves):
-        keys = []
-        for move in moves:
-            object = self.map[move]
-            if self.is_key(object):
-                keys += [object]
-            if self.is_door(object) and object.lower() not in keys:
-                return False
-        return True
-
-    def cache_hit(self, keys):
-        if ''.join(keys) in self.cache:
-            return False
-        self.cache += [''.join(keys)]
-        return True
-
-    def find_location(self, object):
-        if object in self.key_locations:
-            return self.key_locations[object]
-        for pos in self.map:
-            if self.map[pos] == object:
-                self.key_locations[object] = pos
-                return pos
-
-    def is_key(self, object):
-        return not re.search('[a-z]', object) is None
-
-    def is_door(self, object):
-        return not re.search('[A-Z]', object) is None
+    def run(self):
+        current = '@'
+        keys = self.find_keys()
+        self.map_all_keys()
+        self.cache = {}
+        best = self.find_best([current], keys)
+        return best
 
     def find_keys(self):
         if self.keys == []:
@@ -65,13 +39,73 @@ class Vault:
             self.keys = list(sorted(keys))
         return self.keys
 
-    def show_time(self):
-        out = ''
-        secs = time.time() - self.start
-        if secs // 60 // 60 > 0:
-            out += f"{secs // 60 // 60}h"
-        out += f"{secs % 3600 // 60}m{round(secs%60,0)}s"
-        return out
+    def is_key(self, object):
+        return not re.search('[a-z]', object) is None
+
+    def is_door(self, object):
+        return not re.search('[A-Z]', object) is None
+
+    def map_all_keys(self):
+        self.paths = {}
+        keys = self.find_keys() + ['@']
+        print(f"looking for paths for {len(keys)}")
+        for k1 in keys:
+            start = time.time()
+            self.paths[k1] = {}
+            for k2 in keys:
+                if k1 == k2:
+                    continue
+                self.paths[k1][k2] = self.find_path(self.find_location(k1), self.find_location(k2))
+            print(f"found path {k1} = {round(time.time()-start, 1)}s")
+
+    def get_time(self):
+        secs = int(time.time() - self.start)
+        hours = secs // 3600
+        mins = ( secs % 3600 ) // 60
+        secs = secs % 60
+        result = ""
+        if hours > 0:
+            result += f"{hours}h"
+        if mins > 0:
+            result += f"{mins}m"
+        result += f"{secs}s"
+        return result
+
+    def find_location(self, object):
+        if object in self.key_locations:
+            return self.key_locations[object]
+        for pos in self.map:
+            if self.map[pos] == object:
+                self.key_locations[object] = pos
+                return pos
+
+    def get_cache(self, current, keys):
+        key = ''.join([current] + keys)
+        hit = self.cache.get(key, None)
+        return hit
+
+    def set_cache(self, current, keys, result, bestmoves):
+        key = ''.join([current] + keys)
+        if len(key) > self.longest or len(key) >= len(self.keys) + 1:
+            print(f"{len(key)}:{bestmoves} = {result} {self.get_time()} {len(self.cache.keys())}")
+            self.longest = len(key)
+        self.cache[key] = result
+
+    def build_path(self, keys):
+        moves = []
+        for n in range(1, len(keys)):
+            moves += self.paths[keys[n-1]][keys[n]]['moves']
+        return moves
+
+    def valid_path(self, moves):
+        keys = []
+        for move in moves:
+            object = self.map[move]
+            if self.is_key(object):
+                keys += [object]
+            if self.is_door(object) and object.lower() not in keys:
+                return False
+        return True
 
     def find_path(self, start, goal):
         paths = [{'loc': start, 'moves': [], 'doors': []}]
@@ -91,12 +125,6 @@ class Vault:
                     paths = paths[:] + [move]
             paths.sort(key=lambda move: len(move['moves']))
 
-    def print_move(self, move, message=''):
-        moves = move['moves']
-        keys = move['keys']
-        position = move['loc']
-        print(f"{message} moves={moves} position={position} keys={keys}")
-
     def add_if_valid(self, position):
         if position['loc'] in self.map:
             return [position]
@@ -110,81 +138,26 @@ class Vault:
         self.add_if_valid({'loc': (position['loc'][0], position['loc'][1] + 1), 'moves': position['moves'] + [(position['loc'][0], position['loc'][1] + 1)], 'doors': position['doors']})
         return moves
 
-    def map_all_keys(self):
-        self.paths = {}
-        keys = self.find_keys() + ['@']
-        print(f"looking for paths for {len(keys)}")
-        for k1 in keys:
-            start = time.time()
-            self.paths[k1] = {}
-            for k2 in keys:
-                if k1 == k2:
-                    continue
-                self.paths[k1][k2] = self.find_path(self.find_location(k1), self.find_location(k2))
-            print(f"found path {k1} = {round(time.time()-start, 1)}s")
-
-    def update_keys(self, path, keys_left):
-        keys = []
-        for key in path['doors']:
-            if key in keys_left:
-                keys += [key]
-        path['doors'] = keys
-
-    def shortest_key(self, paths):
-        shortest = len(paths[0]['keys'])
-        for path in paths:
-            if len(path['keys']) < shortest:
-                shortest = len(path['keys'])
-        return shortest
-
-    def run(self):
-        current = '@'
-        keys = self.find_keys()
-        self.map_all_keys()
-        paths = [{'current': '@', 'keys': [], 'moves': []}]
-        best = None
-        lastbest = None
-        counter = 0
-        start = time.time()
-        print(f"0 {self.show_time()}")
-        while self.shortest_key(paths) < len(keys):
-            if len(paths[0]['keys']) == len(keys):
-                if not best or len(paths[0]['moves']) < best:
-                    best = len(paths[0]['moves'])
-                    print(f"{counter} paths={len(paths)} best={best} keys={paths[0]['keys']}")
-            paths.sort(key=lambda e: (-1 * len(e['keys']), len(e['moves'])))
-            n = 0
-            while len(paths[n]['keys']) == len(keys) and n < len(paths):
-                if best is not None and len(paths[n]['moves']) > best:
-                    paths.remove(paths[n])
-                else:
-                    n += 1
-            if n == len(paths):
-                return len(paths[0]['moves'])
-            path0 = paths[n]
-            paths.remove(path0)
-            for key in self.keys_left(path0['keys']):
-                path1 = self.paths[path0['current']][key]
-                moves = path0['moves'][:] + path1['moves'][:]
-                keys1 = self.collect_keys(moves)
-                new_path = {'current':key, 'keys': keys1, 'moves': moves}
-                if self.valid_path(moves) and self.cache_hit(keys1):
-                    paths += [new_path]
-            counter += 1
-            if counter % 5000 == 0:
-                print(f"{counter} {len(paths)} {round(time.time()-start,1)}s {self.show_time()}")
-                start = time.time()
-        return len(paths[0]['moves'])
-
-    def parse_map(self, text):
-        y = 0
-        for line in text.split('\n'):
-            x = 0
-            for item in list(line):
-                if re.search('[A-Za-z.@]', item):
-                    self.map[(x,y)] = item
-                x += 1
-            y += 1
+    def find_best(self, moves, keys):
+        if len(keys) == 0:
+            return 0
+        current = moves[-1]
+        cached = self.get_cache(current, keys)
+        if cached:
+            return cached
+        best = 99999999
+        for key in keys:
+            moves1 = moves[:] + [key]
+            if not self.valid_path(self.build_path(moves1)):
+                continue
+            keys0 = keys[:]
+            keys0.remove(key)
+            newlen = self.find_best(moves1, keys0) + len(self.paths[current][key]['moves'])
+            if newlen < best:
+                best = newlen
+                bestmoves = moves1
+            self.set_cache(current, keys, best, bestmoves)
+        return best
 
 
 def test_1():
@@ -218,22 +191,7 @@ def test_2():
     assert result == 86
 
 
-# def test_3():
-#     vault = Vault("""
-# #################
-# #i.G..c...e..H.p#
-# ########.########
-# #j.A..b...f..D.o#
-# ########@########
-# #k.E..a...g..B.n#
-# ########.########
-# #l.F..d...h..C.m#
-# #################
-# """)
-#     result = vault.run()
-#     assert result == 136
-
-def test_4():
+def test_3():
     vault = Vault("""
 ########################
 #...............b.C.D.f#
@@ -244,7 +202,7 @@ def test_4():
     result = vault.run()
     assert result == 132
 
-def test_5():
+def test_4():
     vault = Vault("""
 ########################
 #@..............ac.GI.b#
@@ -255,6 +213,22 @@ def test_5():
 """)
     result = vault.run()
     assert result == 81
+
+def test_5():
+    vault = Vault("""
+#################
+#i.G..c...e..H.p#
+########.########
+#j.A..b...f..D.o#
+########@########
+#k.E..a...g..B.n#
+########.########
+#l.F..d...h..C.m#
+#################
+""")
+    result = vault.run()
+    assert result == 136
+
 
 if __name__ == '__main__':
     vault = Vault("""
@@ -339,16 +313,5 @@ if __name__ == '__main__':
 ###.#####.###.#.#.###########.###.#.#.#.#.#.#.#.###.###.###.#.#####.#F#####.#.#.#
 #.......#.......#.............#...#.....#.#...#.........#.....#.............#i..#
 #################################################################################""")
-#     vault = Vault("""
-# #################
-# #i.G..c...e..H.p#
-# ########.########
-# #j.A..b...f..D.o#
-# ########@########
-# #k.E..a...g..B.n#
-# ########.########
-# #l.F..d...h..C.m#
-# #################
-# """)
     result = vault.run()
-    print(result)
+    print(result)  # 3586  16h
