5264ff290f3d51bcf49a59e2fe65c23c0de71f8c
Dan Schaffer <schafferdan@gmail.com>
2020-12-15 08:09:47 -0500

    day15 part2 initial solution

--- 2020/day15.py
+++ 2020/day15.py
@@ -14,8 +14,16 @@ def run_part1(numbers, rounds):
         numbers += [(match+1)]
     return numbers[-1]
 
-def run_part2():
-    return -1
+def run_part2(numbers, rounds):
+    if os.path.exists(numbers):
+        numbers = [int(number) for number in open(numbers).read().strip().split(',')]
+    else:
+        numbers = [int(number) for number in numbers.split(',')]
+    memory = {n: i+1 for i,n in enumerate(numbers[:-1])}
+    for i in range(len(numbers), rounds):
+        numbers.append(i-memory.get(numbers[-1], i))
+        memory[numbers[-2]] = i
+    return numbers[-1]
 
 def test1():
     assert run_part1('0,3,6', 4) == 0
@@ -27,7 +35,8 @@ def test1():
     assert run_part1('0,3,6', 10) == 0
     assert run_part1('1,3,2', 2020) == 1
     assert run_part1('./day15.input', 2020) == 1025
-
+    assert run_part1('0,3,6', 30000000) == 175594
 if __name__ == '__main__':
     print("advent of code: day15")
     print(f"part 1: {run_part1('./day15.input', 2020)}")
+    print(f"part 2: {run_part2('./day15.input', 30000000)}")
