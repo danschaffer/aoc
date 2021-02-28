4b380cabbf9f4c1fc0305abfc55fc7b76b161609
Dan Schaffer <schafferdan@gmail.com>
2021-02-28 10:21:01 -0500

    template updates

--- template.py
+++ template.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
 class Day:
-    def __init__(self):
-        self.lines = []
-
-    def load(self, file):
-#        self.lines = open(file).read().strip().split('\n')
-        pass
+    def __init__(self, file):
+        self.data = {}
+        for line in open(file).readlines():
+            parts = line.split()
 
     def run_part1(self):
         return -1
@@ -15,14 +13,12 @@ class Day:
         return -1
 
 def test1():
-    test_day = Day()
-    test_day.load('./day.input')
+    test_day = Day('./day-test.input')
     assert test_day.run_part1() == -1
     assert test_day.run_part2() == -1
 
 if __name__ == '__main__':
     print("advent of code: day")
-    day = Day()
-    day.load('./day.input')
+    day = Day('./day.input')
     print(f"part 1: {day.run_part1()}")
     print(f"part 2: {day.run_part2()}")
