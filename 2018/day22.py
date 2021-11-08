41607dd623be995127e77d917e7cbc741f3dc8a7
Dan Schaffer <dschaffer@circle.com>
2021-11-08 17:58:30 -0500

    add 2018 day 18 p1 and p2

--- 2018/day22.py
+++ 2018/day22.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
-import heapq
-from queue import PriorityQueue
-
+import networkx as nx
 class Day22:
-    def __init__(self, depth, target):
+    def __init__(self, depth, target_x, target_y):
         self.depth = depth
-        self.target_x = target[0]
-        self.target_y = target[1]
+        self.target_x = target_x
+        self.target_y = target_y
         self.table = {}
 
     def print_board(self):
@@ -35,14 +33,49 @@ class Day22:
                 answer += erosion_level % 3
         return answer
 
+    def run_part2(self, x, y):
+        rocky, wet, narrow = 0,1,2
+        torch, gear, neither = 0,1,2
+        graph = nx.Graph()
+        for x in range(x+1):
+            for y in range(y+1):
+                risk = self.table[(x,y)] % 3
+                if risk == rocky:
+                    graph.add_edge((x,y,torch),(x,y,gear),weight=7)
+                elif risk == wet:
+                    graph.add_edge((x,y,gear),(x,y,neither),weight=7)
+                else:
+                    graph.add_edge((x,y,torch),(x,y,neither),weight=7)
+                for dx, dy in ((0,1),(0,-1),(1,0),(-1,0)):
+                    newx,newy = x+dx, y+dy
+                    if newx < 0 or newx > x or newy < 0 or newy > y:
+                        continue
+                    risk = self.table[(newx,newy)] % 3
+                    if risk == rocky:
+                        graph.add_edge((x,y,gear), (newx,newy,gear),weight=1)
+                        graph.add_edge((x,y,torch), (newx,newy,torch),weight=1)
+                    elif risk == wet:
+                        graph.add_edge((x,y,gear), (newx,newy,gear),weight=1)
+                        graph.add_edge((x,y,neither), (newx,newy,neither),weight=1)
+                    else:
+                        graph.add_edge((x,y,torch), (newx,newy,torch),weight=1)
+                        graph.add_edge((x,y,neither), (newx,newy,neither),weight=1)
+        for i,path in enumerate(nx.dijkstra_path(graph,(0,0,torch),(x,y,torch))):
+            print(f"{i} {path}")
+        return nx.dijkstra_path_length(graph,(0,0,torch),(x,y,torch))
+
     def get_distance(self, loc, goal):
         return abs(loc[0] - goal[0]) + abs(loc[1] - goal[1])
 
 def test1():
     assert Day22(510,(10,10)).run_part1() == 114
+
+def test2():
     assert Day22(7305,(13,734)).run_part1() == 10204
 
 if __name__ == '__main__':
     print("advent of code: day22")
-    print(f"part 1: {Day22(7305,(13,734)).run_part1()}")
+    d = Day22(7305,13,734)
+    print(f"part 1: {d.run_part1()}")
+    print(f"part 2: {d.run_part2(13,734)}")
 
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ total: 290
 
 - 2020 50 points, needed lots of help on about 5 problems
 - 2019 50 points, needed help of several solutions, trouble with 20,22,24, Nov 2021
-- 2018 40 points, in progress
+- 2018 42 points, in progress, struggling with 15,21,22,24
 - 2017 50 points, completed all solutions in Feb 2021
 - 2016 50 points, completed all solutions in Feb 2021
 - 2015 50 points, completed all solutions in Nov 2020
