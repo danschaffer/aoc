7aaa569c104ed6a1c78a7fe62a7615797422bfea
Dan Schaffer <dschaffer@circle.com>
2021-12-01 06:42:29 -0500

    2021 day1 cleanup

--- 2021/day01.py
+++ 2021/day01.py
@@ -11,12 +11,12 @@ class Day1:
         return sum([1 for n in range(len(self.data)-3) if self.data[n]+self.data[n+1]+self.data[n+2]<self.data[n+1]+self.data[n+2]+self.data[n+3]])
 
 def test1():
-    assert Day1('day1-test.inp').part1() == 7
-    assert Day1('day1.inp').part1() == 1301
-    assert Day1('day1-test.inp').part2() == 5
-    assert Day1('day1.inp').part2() == 1346
+    assert Day1('day01-test.input').part1() == 7
+    assert Day1('day01.input').part1() == 1301
+    assert Day1('day01-test.input').part2() == 5
+    assert Day1('day01.input').part2() == 1346
 
 if __name__ == '__main__':
     print("aoc 2021 day 1")
-    print(f"part 1: {Day1('day1.inp').part1()}")
-    print(f"part 2: {Day1('day1.inp').part2()}")
+    print(f"part 1: {Day1('day01.input').part1()}")
+    print(f"part 2: {Day1('day01.input').part2()}")
