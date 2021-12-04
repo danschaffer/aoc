4789e87305f757b990cf528d3b632e15c88a257b
Dan Schaffer <dschaffer@circle.com>
2021-12-04 12:01:46 -0500

    refactor day 4

--- 2021/day04.py
+++ 2021/day04.py
@@ -4,8 +4,11 @@ class Day04:
     def __init__(self, file):
         self.numbers = []
         self.boards = []
+        self.lines = self.load(file)
+    
+    def load(self, file):
         board = []
-        self.lines = {}
+        lines = {}
         for line in open(file).readlines():
             if not self.numbers:
                 self.numbers = [int(number) for number in line.split(',')]
@@ -19,25 +22,12 @@ class Day04:
             self.boards.append(board)
         for i in range(len(self.boards)):
             for j in range(5):
-                self.lines[f"{i}c{j}"] = 0
-                self.lines[f"{i}r{j}"] = 0
+                lines[f"{i}c{j}"] = 0
+                lines[f"{i}r{j}"] = 0
+        return lines
 
-    def run_part1(self):
-        while True:
-            number = self.numbers.pop(0)
-            for n,board in enumerate(self.boards):
-                if number in self.boards[n]:
-                    loc = board.index(number)
-                    row = loc // 5
-                    col = loc % 5
-                    self.lines[f"{n}c{col}"] += 1
-                    self.lines[f"{n}r{row}"] += 1
-                    board[loc] = -1
-                    if self.lines[f"{n}c{col}"] == 5 or self.lines[f"{n}r{row}"] == 5:
-                        return sum([n for n in board if n > -1])*number
-            assert self.numbers
-
-    def run_part2(self):
+    def run(self):
+        part1 = None
         boards_won = set()
         while True:
             number = self.numbers.pop(0)
@@ -51,25 +41,27 @@ class Day04:
                     board[loc] = -1
                     if self.lines[f"{n}c{col}"] == 5 or self.lines[f"{n}r{row}"] == 5:
                         boards_won.add(n)
+                        answer = sum([b for b in board if b > -1])*number
+                        if part1 is None:
+                            part1 = answer
                         if len(boards_won) == len(self.boards):
-                            return sum([n0 for n0 in board if n0 > -1])*number
-            assert self.numbers
+                            return part1, answer
 
 def test1():
     test_day04 = Day04('./day04-test.input')
-    assert test_day04.run_part1() == 4512
-    test_day04 = Day04('./day04-test.input')
-    assert test_day04.run_part2() == 1924
+    part1, part2 = test_day04.run()
+    assert part1 == 4512
+    assert part2 == 1924
 
 def test2():
     test_day04 = Day04('./day04.input')
-    assert test_day04.run_part1() == 38594
-    test_day04 = Day04('./day04.input')
-    assert test_day04.run_part2() == 21184
+    part1, part2 = test_day04.run()
+    assert part1 == 38594
+    assert part2 == 21184
 
 if __name__ == '__main__':
     print("advent of code: day04")
     day04 = Day04('./day04.input')
-    print(f"part 1: {day04.run_part1()}")
-    day04 = Day04('./day04.input')
-    print(f"part 2: {day04.run_part2()}")
+    part1, part2 = day04.run()
+    print(f"part 1: {part1}")
+    print(f"part 2: {part2}")
