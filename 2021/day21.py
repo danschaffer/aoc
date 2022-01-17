e332120999e60ce56417c34b228a4f78cd7a821e
Dan Schaffer <dschaffer@circle.com>
2022-01-17 11:55:32 -0500

    day 21

--- 2021/day21.py
+++ 2021/day21.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+import functools
+import itertools
 class Day21:
     def __init__(self, file):
         self.data = {}
@@ -44,21 +45,40 @@ class Day21:
             if self.score2 >= 1000:
                 return self.score1 * self.rolls
 
+    @functools.cache
+    def count_wins(self, position_1, position_2, score_1, score_2):
+        wins_1 = 0
+        wins_2 = 0
+        for roll_1, roll_2, roll_3 in itertools.product((1,2,3), repeat=3):
+            new_position_1 = (position_1 - 1 + roll_1 + roll_2 + roll_3) % 10 + 1
+            new_score_1 = score_1 + new_position_1
+            if new_score_1 >= 21:
+                wins_1 += 1
+            else:
+                new_wins_2, new_wins_1 = self.count_wins(position_2, new_position_1, score_2, new_score_1)
+                wins_1 += new_wins_1
+                wins_2 += new_wins_2
+        return wins_1, wins_2
+
     def run_part2(self):
-        return -1
+        scores = self.count_wins(self.player1, self.player2, 0, 0)
+        return max(scores)
 
 def test1():
     test_day21 = Day21('./day21-test.input')
     assert test_day21.run_part1() == 739785
+    test_day21 = Day21('./day21-test.input')
     assert test_day21.run_part2() == 444356092776315
 
 def test2():
     test_day21 = Day21('./day21.input')
     assert test_day21.run_part1() == 597600
-    assert test_day21.run_part2() == -1
+    test_day21 = Day21('./day21.input')
+    assert test_day21.run_part2() == 634769613696613
 
 if __name__ == '__main__':
     print("advent of code: day21")
     day21 = Day21('./day21.input')
     print(f"part 1: {day21.run_part1()}")
+    day21 = Day21('./day21.input')
     print(f"part 2: {day21.run_part2()}")
