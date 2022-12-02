036f7cb2b2e76953de64e9ff1933d6f91788f81e
Dan Schaffer <dschaffer@circle.com>
2022-12-02 08:10:49 -0500

    refactor day 2

--- 2022/day02.py
+++ 2022/day02.py
@@ -4,51 +4,44 @@ class Day02:
     def __init__(self, file):
         self.file = file
 
-    def run_part1(self):
+    def run(self):
         # A rock, B paper, C scissors
         # X rock, Y paper, Z scissors
         # rock beats scissors
         # scissors beats paper
         # paper beats rock
-        answer = 0
-        mescore = {'X': 1, 'Y': 2, 'Z': 3}
+        answer1 = answer2 = 0
+        my_score = {'X': 1, 'Y': 2, 'Z': 3}
+        lose_score = {'A': 3,'B': 1, 'C': 2}
+        tie_score = {'A': 1, 'B': 2, 'C': 3}
+        win_score = {'A': 2, 'B': 3, 'C': 1}
         for line in open(self.file).readlines():
             opp, me = line.split()
-            answer += mescore[me]
+            answer1 += my_score[me]
             if opp == 'A' and me == 'X' or opp == 'B' and me == 'Y' or opp == 'C' and me == 'Z':
-                answer += 3
+                answer1 += 3
             elif me == 'X' and opp == 'C' or me == 'Z' and opp == 'B' or me == 'Y' and opp == 'A':
-                answer += 6
-        return answer
-
-    def run_part2(self):
-        # X lose, Y draw, Z win
-        answer = 0
-        losescore = {'A': 3,'B': 1, 'C': 2}
-        tiescore = {'A': 1, 'B': 2, 'C': 3}
-        winscore = {'A': 2, 'B': 3, 'C': 1}
-        for line in open(self.file).readlines():
-            opp, me = line.split()
+                answer1 += 6
             if me == 'X': # lose
-                answer += losescore[opp]
+                answer2 += lose_score[opp]
             elif me == 'Y': # tie
-                answer += 3 + tiescore[opp]
+                answer2 += 3 + tie_score[opp]
             elif me == 'Z': # win
-                answer += 6 + winscore[opp]
-        return answer
+                answer2 += 6 + win_score[opp]
+        return answer1, answer2
 
 def test1():
-    test_day02 = Day02('./day02-test.input')
-    assert test_day02.run_part1() == 15
-    assert test_day02.run_part2() == 12
+    answer1, answer2 = Day02('./day02-test.input').run()
+    assert answer1 == 15
+    assert answer2 == 12
 
 def test2():
-    test_day02 = Day02('./day02.input')
-    assert test_day02.run_part1() == 8392
-    assert test_day02.run_part2() == 10116
+    answer1, answer2 = Day02('./day02.input').run()
+    assert answer1 == 8392
+    assert answer2 == 10116
 
 if __name__ == '__main__':
     print("advent of code: day02")
-    day02 = Day02('./day02.input')
-    print(f"part 1: {day02.run_part1()}")
-    print(f"part 2: {day02.run_part2()}")
+    answer1, answer2 = Day02('./day02.input').run()
+    print(f"part 1: {answer1}")
+    print(f"part 2: {answer2}")
