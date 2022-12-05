17a143debe1fe5b94515eaede2db5b7fd82ee2ab
Dan Schaffer <dschaffer@circle.com>
2022-12-05 12:53:25 -0500

    refactor day5

--- 2022/day05.py
+++ 2022/day05.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import copy
 import sys
 class Day05:
     def __init__(self, file):
@@ -24,40 +25,34 @@ class Day05:
                 parts = line.split()
                 self.moves.append((int(parts[1]), int(parts[3])-1, int(parts[5])-1))
 
-    def run_part1(self):
-        self.data1 = self.data[:]
-        self.data2 = self.data[:]
-        for (num, _from, to) in self.moves:
-            for _ in range(num):
-                item = self.data[_from].pop(0)
-                self.data[to].insert(0, item)
-        return ''.join([item[0] for item in self.data])
-
-    def run_part2(self):
-        for (num, _from, to) in self.moves:
-            for n in range(num):
-                item = self.data[_from].pop(0)
-                self.data[to].insert(n, item)
-        return ''.join([item[0] for item in self.data])
+    def run(self):
+        data1 = copy.deepcopy(self.data)
+        data2 = copy.deepcopy(self.data)
+        for (num, src, dest) in self.moves:
+            for i in range(num):
+                item1 = data1[src].pop(0)
+                data1[dest].insert(0, item1)
+                item2 = data2[src].pop(0)
+                data2[dest].insert(i, item2)
+        answer1 = ''.join([item[0] for item in data1])
+        answer2 = ''.join([item[0] for item in data2])
+        return answer1, answer2
 
 def test1():
-    test_day05 = Day05('./day05-test.input')
-    assert test_day05.run_part1() == 'CMZ'
-    test_day05 = Day05('./day05-test.input')
-    assert test_day05.run_part2() == 'MCD'
+    answer1, answer2 = Day05('./day05-test.input').run()
+    assert answer1 == 'CMZ'
+    assert answer2 == 'MCD'
 
 def test2():
-    test_day05 = Day05('./day05.input')
-    assert test_day05.run_part1() == 'HNSNMTLHQ'
-    test_day05 = Day05('./day05.input')
-    assert test_day05.run_part2() == 'RNLFDJMCT'
+    answer1, answer2 = Day05('./day05.input').run()
+    assert answer1 == 'HNSNMTLHQ'
+    assert answer2 == 'RNLFDJMCT'
 
 if __name__ == '__main__':
     print("advent of code: day05")
     file = './day05.input'
     if len(sys.argv) > 1:
         file = sys.argv[1]
-    day05 = Day05(file)
-    print(f"part 1: {day05.run_part1()}")
-    day05 = Day05(file)
-    print(f"part 2: {day05.run_part2()}")
+    answer1, answer2 = Day05(file).run()
+    print(f"part 1: {answer1}")
+    print(f"part 2: {answer2}")
